341f719864b53383dceb4feed46b5f36d6594e96
Max <realmrmax@gmail.com>
2020-04-29 12:44:36 +0300

    'start'

---
+++ plugin.rb
@@ -0,0 +1,6 @@
+# name: discourse_featuredlink_rewriter
+# about: Replace featured link text
+# version: 0.1
+# authors: Maxym Khaykin
+# url: https://github.com/realmrmax/discourse_featuredlink_rewriter
+
